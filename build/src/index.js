50444d87ba7092d9e5fb8fa643e5a182bbd55f50
umurcan <me@umurcan.dev>
2024-01-28 06:40:06 +0300

    🔧 chore(.gitignore): add 'build' directory to ignore list to exclude build artifacts from version control
    🔧 chore(package.json): update 'start' script to correctly pass environment variables to the application
    
    🔧 chore(package.json): add '@types/pg' as a dev dependency to provide type definitions for PostgreSQL
    
    🔧 chore(package.json): add 'pg' as a dependency to enable PostgreSQL database connectivity
    
    🔧 chore(src/database/db.ts): create a new file to define a PostgreSQL client and export it
    
    🔧 chore(src/index.ts): import and connect the PostgreSQL client in the main application file

--- build/src/index.js
+++ build/src/index.js
@@ -4,11 +4,31 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const db_1 = __importDefault(require("./database/db"));
+db_1.default.connect();
 const app = (0, express_1.default)();
-const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
+const PORT = process.env.PORT;
+app.get('/', async (req, res) => {
+    try {
+        const result = await db_1.default.query('SELECT * FROM visits');
+        res.json(result.rows);
+    }
+    catch (error) {
+        console.error('Error executing query:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
+async function test() {
+    console.log('sa');
+    try {
+        const result = await db_1.default.query('SELECT * FROM visits');
+        console.log(result.rows);
+    }
+    catch (e) {
+        console.error('Error executing query:', e);
+    }
+}
+test();
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,20 @@
 import express, {Request, Response} from 'express';
+import client from './database/db';
 
+client.connect();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, world!');
+app.get('/', async (req: Request, res: Response) => {
+  try {
+    const result = await client.query('SELECT * FROM visits');
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({error: 'Internal server error'});
+  }
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
